fa3039b471faf39f95b37c6749b7b760ee6a560c
Lucas <iamsamuelfak@gmail.com>
2025-03-08 20:44:42 +0100

    chore: isolate data state

--- src/app/(home)/screens/index.tsx
+++ src/app/(home)/screens/index.tsx
@@ -28,8 +28,9 @@ interface Item {
 }
 
 export const ReceiptGenerator = () => {
-  const [total, setTotal] = useState(0)
   const [data, setData] = useState(initialState)
+
+  const [total, setTotal] = useState(0)
   const [items, setItems] = useState([{ name: '', quantity: 1, price: 0 }])
 
   const {
